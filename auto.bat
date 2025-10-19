c13aacc1e0fc2beca4ba6dec2a56aeaa125abc47
Dung Hoang Dev 02 <dung12311vn@gmail.com>
2025-10-19 20:31:41 +0700

    Update auto.bat

--- auto.bat
+++ auto.bat
@@ -1,5 +1,4 @@
 @echo off
-:: Kiểm tra quyền admin
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo [!] Dang yeu cau quyen Administrator...
@@ -7,7 +6,6 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-:: Tải file Python mới nhất từ GitHub (raw)
 echo [*] Dang tai sleep.py tu GitHub...
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/zenixbot0101/FIX-SHUTDOWN-RDP/refs/heads/main/sleep.py' -OutFile 'C:\sleep.py'" >nul 2>&1
 
@@ -19,7 +17,6 @@ if exist C:\sleep.py (
     exit /b
 )
 
-:: Chạy Python
 echo [*] Dang khoi dong AFK MODE...
 python C:\sleep.py
 pause
